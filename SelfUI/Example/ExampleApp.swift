775a9bc8071bf64f05d4793b2fe9c7d9f603e40d
Long Pham <philong.bmt@gmail.com>
2024-05-14 14:19:49 +0700

    project initial

---
+++ SelfUI/Example/ExampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Long Pham on 14/5/24.
+//
+
+import SwiftUI
+import SelfUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PageContentView()
+        }
+    }
+}
---
+++ SelfUI/SelfUI/PageContentView.swift
@@ -0,0 +1,21 @@
+//
+//  PageContentView.swift
+//  SelfUI
+//
+//  Created by Long Pham on 14/5/24.
+//
+
+import SwiftUI
+
+public struct PageContentView: View {
+    
+    public init() {}
+    
+    public var body: some View {
+        Text("Hello, SelfUI")
+    }
+}
+
+#Preview {
+    PageContentView()
+}
