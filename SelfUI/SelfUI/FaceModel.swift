c8fa9514d5d4dbb400e08ecf897b592f8188f61f
Long Pham <philong.bmt@gmail.com>
2024-05-16 11:37:19 +0700

    add utils files and face model [WIP]

---
+++ SelfUI/SelfUI/FaceModel.swift
@@ -0,0 +1,16 @@
+//
+//  FaceModel.swift
+//  SelfUI
+//
+//  Created by Long Pham on 16/5/24.
+//
+
+import SwiftUI
+import AVFoundation
+import MLKit
+
+// Model
+struct FaceModel {
+    let frame: CGRect // The frame of the detected face
+    // Add other properties as needed
+}
