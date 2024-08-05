60f1fb54627c9742350935f121ca9261aedff83d
Long Pham <philong.bmt@gmail.com>
2024-08-05 15:18:57 +0700

    creating account [WIP]

---
+++ SelfUI/SelfUI/Create account/CreateAccountFlow.swift
@@ -0,0 +1,41 @@
+//
+//  CreateAccountFlow.swift
+//  SelfUI
+//
+//  Created by Long Pham on 5/8/24.
+//
+
+import SwiftUI
+
+public struct CreateAccountFlow: BaseView {
+    func loadFonts() {
+        FontLoader.loadFont()
+    }
+    
+    @State private var path = [Int]()
+    var onFinish: ((Bool) -> Void)?
+    
+    public init(onFinish: ( (Bool) -> Void)? = nil) {
+        self.onFinish = onFinish
+    }
+    
+    public var body: some View {
+        NavigationStack(path: $path) {
+            CreateAccountNameView { name in
+                path = [0]
+            }.navigationDestination(for: Int.self) { selection in
+                switch selection {
+                case 0:
+                    CreatingAccountView()
+                    
+                default:
+                    Text("0")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    CreateAccountFlow()
+}
---
+++ SelfUI/SelfUI/Create account/CreateAccountNameView.swift
@@ -0,0 +1,76 @@
+//
+//  CreateAccountNameView.swift
+//  SelfUI
+//
+//  Created by Long Pham on 5/8/24.
+//
+
+import SwiftUI
+
+struct CreateAccountNameView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @State private var name: String = ""
+    @State private var isValid: Bool = false
+    
+    public init(onEnteredName: ((String) -> Void)? = nil) {
+        self.onEnteredName = onEnteredName
+    }
+    
+    var onEnteredName: ((String) -> Void)?
+    
+    public var body: some View {
+        VStack {
+            // stepped progress view
+            CustomProgressView(steps: [
+                Step(title: "1", state: .done),
+                Step(title: "2", state: .done),
+                Step(title: "3", state: .done),
+                Step(title: "4", state: .done),
+                Step(title: "5", state: .active)
+            ])
+            
+            Spacer(minLength: 50)
+            VStack(alignment: .leading, spacing: 30) {
+                Text("create_account_name_title".localized)
+                    .font(.defaultTitle)
+                    .foregroundColor(.black)
+                
+                OutlinedTextField(boxLabel: "enter_name_label".localized, placeHolder: "enter_name_placeholder".localized, boxErrorDescription: "enter_name_error".localized, text: $name, isValid: $isValid)
+                    .onChange(of: name) { newValue in
+                        isValid = newValue.count > 0
+                    }
+                
+                Spacer()
+            }
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            Spacer()
+            VStack(spacing: 12) {
+                ButtonView(title: "button_confirm_name".localized,backgroundColor:  .defaultDark) {
+                    onEnteredName?(name)
+                }.disabled(!isValid)
+                
+                BrandView(isDarked: true)
+            }.padding()
+        }
+        .background(.white)
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button(action: {
+                    presentationMode.wrappedValue.dismiss()
+                }) {
+                    HStack {
+                        Image("ic_back_dark", bundle: mainBundle)
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    CreateAccountNameView()
+}
