baa9c0673daaedba39ff11118a54bfb0ed516f36
Long Pham <philong.bmt@gmail.com>
2024-10-03 13:16:58 +0700

    set line limit for message list cell

--- SelfUI/SelfUI/Messaging/MessageListCell.swift
+++ SelfUI/SelfUI/Messaging/MessageListCell.swift
@@ -14,6 +14,7 @@ public struct MessageListCell: View {
     let message: String
     let timestamp: String
     let badge: String
+    private let lineLimit = 2
     
     public init (title: String = "",
                  message: String = "",
@@ -34,6 +35,7 @@ public struct MessageListCell: View {
                 VStack (alignment: .leading)  {
                     // Heading/H5
                     Text(title)
+                        .lineLimit(lineLimit)
                         .font(
                             Font.custom("Barlow", size: 21)
                                 .weight(.bold)
@@ -43,6 +45,7 @@ public struct MessageListCell: View {
                     // Subtitle/Subtitle 1
                     Text(message)
                         .font(Font.custom("Barlow", size: 17))
+                        .lineLimit(lineLimit)
                         .foregroundColor(Color(red: 0.38, green: 0.42, blue: 0.48))
                 }
                 Spacer()
@@ -81,7 +84,7 @@ public struct MessageListCell: View {
     ZStack {
         Color.white.ignoresSafeArea()
         List {
-            MessageListCell(title: "Portal", message: "Last message", timestamp: "Yesterday", badge: "23")
+            MessageListCell(title: "Portal", message: "I am going to do it. I have made up my mind. These are the first few words of the new... the best ... the Longest Text In The Entire History Of The Known Universe! This Has To Have Over 35,000 words the beat the current world record set by that person who made that flaming chicken handbooky thingy. I might just be saying random things the whole time I type in this so you might get confused a lot. I just discovered something terrible. autocorrect is on!! no!!! this has to be crazy, so I will have to break all the English language rules and the basic knowledge of the average human being. I am not an average human being, however I am special. no no no, not THAT kind of special ;). Why do people send that wink face! it always gives me nightmares! it can make a completely normal sentence creepy. imagine you are going to a friend’s house, so you text this: [ see you soon ] seems normal, right? But what is you add the word semi to that colon?", timestamp: "Yesterday", badge: "23")
             MessageListCell(title: "Custom App", message: "Last message", timestamp: "Now", badge: "")
         }.listStyle(.plain)
     }
