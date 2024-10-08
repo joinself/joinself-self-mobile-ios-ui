60448425068d0fa433009cbbe2f10a9a137377fb
Long Pham <philong.bmt@gmail.com>
2024-10-08 08:45:24 +0700

    add some more message type

--- SelfUI/SelfUI/DTO/MessageDTO.swift
+++ SelfUI/SelfUI/DTO/MessageDTO.swift
@@ -9,7 +9,28 @@ import Foundation
 
 public struct MessageType {
     static let SELF_TEXT = "self/text"
+    static let SELF_CALL = "self/call"
     static let SELF_IMAGE = "self/image"
+    static let SELF_VIDEO = "self/video"
+    static let SELF_AUDIO = "self/audio"
+    static let SELF_LINK = "self/link"
+    static let SELF_FILE = "self/file"
+    static let SELF_CONNECTION = "self/connection"
+    
+    static let GROUP_JOIN = "self/group_join"
+    static let GROUP_LEAVE = "self/group_leave"
+    static let SELF_CREDENTIAL_REQUEST = "self/credential_request"
+    static let SELF_CREDENTIAL = "self/credential"
+    static let SELF_DOCUMENT_SIGN = "self/document_sign"
+    static let SELF_ACKNOWLEDGE = "self/acknowledge"
+    static let SELF_APPROVAL = "self/approval"
+    static let SELF_NOTIFICATION = "self/notification"
+    static let SELF_ACCOUNT_DELETION = "self/account_deletion"
+    static let SELF_CONNECTION_VERIFICATION = "self/connection_verification"
+    static let SELF_CONNECTION_UNVERIFIED = "self/connection_unverified"
+    static let SELF_FORM = "self/form"
+    static let SELF_FORM_RESPONSE = "self/form_response"
+    static let SELF_BUTTONS = "self/commands"
 }
 
 public enum MessageFrom: String, CaseIterable {
