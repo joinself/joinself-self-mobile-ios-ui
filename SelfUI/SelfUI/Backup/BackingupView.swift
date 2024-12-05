823a520a42c8f3513ea939196946288b3c70a227
Long Pham <philong.bmt@gmail.com>
2024-12-05 20:56:50 +0700

    update backing view

--- SelfUI/SelfUI/Backup/BackingupView.swift
+++ SelfUI/SelfUI/Backup/BackingupView.swift
@@ -20,27 +20,10 @@ public struct BackingupView: View {
     @Environment(\.presentationMode) var presentationMode
     
     public var body: some View {
-        VStack(alignment: .center, spacing: 0) {
+        BaseView {
             Spacer()
             LoadingDotsView()
-            
             Spacer()
-            VStack(spacing: 12) {
-                ButtonView(title: "button_backup_now".localized) {
-                    onGettingStarted()
-                }.hidden()
-                
-                BrandView(isDarked: true)
-            }.padding()
-        }
-        .background(.white)
-        .navigationBarBackButtonHidden(true)
-        .toolbar {
-            ToolbarItem(placement: .navigationBarLeading) {
-                NavBackButton(isWhiteBackground: false) {
-                    presentationMode.wrappedValue.dismiss()
-                }
-            }
         }
     }
 }
