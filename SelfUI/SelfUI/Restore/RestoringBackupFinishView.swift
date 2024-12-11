25b4791b9490d6fe6218014eed08070ef5c17b3c
Long Pham <philong.bmt@gmail.com>
2024-12-11 17:33:47 +0700

    restore backup finish view

---
+++ SelfUI/SelfUI/Restore/RestoringBackupFinishView.swift
@@ -0,0 +1,40 @@
+//
+//  RestoringBackupFinishView.swift
+//  SelfUI
+//
+//  Created by Long Pham on 30/5/24.
+//
+
+import SwiftUI
+
+public struct RestoringBackupFinishView: View, BaseActions {
+    var onNext: (() -> Void)?
+    
+    public init(onNext: (() -> Void)? = nil) {
+        self.onNext = onNext
+    }
+    
+    public var body: some View {
+        BaseProgressView (totalSteps: 5, activeStep: 6, content: {
+            VStack(alignment: .leading, spacing: 30) {
+                Text("title_restore_backup_finish_intro".localized)
+                    .font(.defaultLargeTitle)
+                    .foregroundColor(.textPrimary)
+                Text("msg_restore_backup_finish_intro".localized)
+                    .font(Font.defaultBody)
+                  .lineSpacing(1.14)
+                  .foregroundColor(.textPrimary)
+                Spacer()
+                ButtonView(title: "button_turn_on_notifications".localized) {
+                    PermissionHelper.requestNotificationPermission { success in
+                        
+                    }
+                }
+            }
+        })
+    }
+}
+
+#Preview {
+    RestoringBackupFinishView()
+}
