702ff2168d04932f3cea5964a40ffff191228a53
Long Pham <philong.bmt@gmail.com>
2025-02-18 15:36:05 +0700

    update navigation view to support env switcher

--- SelfUI/Components/NavigationTitleView.swift
+++ SelfUI/Components/NavigationTitleView.swift
@@ -11,38 +11,29 @@ public struct NavigationTitleView: View {
     let title: String
     @Binding var font: Font
     let imageURL: URL?
-    public init(title: String, font: Binding<Font> = .constant(.navigationTitle), imageURL: URL? = nil) {
+    @Binding var isOn: Bool
+    public init(title: String, isOn: Binding<Bool> = .constant(false), font: Binding<Font> = .constant(.navigationTitle), imageURL: URL? = nil) {
         self.title = title
         self.imageURL = imageURL
         self._font = font
+        self._isOn = isOn
     }
     public var body: some View {
         HStack {
-            if let imageURL = imageURL {
-                AvatarView(imageURL: imageURL, avatarSize: 30)
-            } else {
-                Image("ic_self_logo", bundle: mainBundle)
-                    .frame(width: 30, height: 30)
+            Toggle(isOn: $isOn) {
+                Text(title)
+                    .foregroundStyle(Color.textPrimary)
+                    .font(.defaultNormalTitle)
             }
-            
-            Text(title)
-                .font(font)
-                .foregroundStyle(Color.textPrimary)
-//                .font(.customLargeTitle)
-//                .fontWeight(.semibold)
-//                .kerning(0.5)
-//                .foregroundStyle(Color.textPrimary)
-            
+            .toggleStyle(ImageToggleStyle(onColor: .defaultGray, offColor: .defaultGray, thumbImageOn: Image("ic_self_logo", bundle: mainBundle), thumbImageOff: Image("ic_logo_quaternary", bundle: mainBundle)))
             Spacer()
         }
-        .frame(maxWidth: .infinity)
-        
     }
 }
 
 #Preview {
     ZStack {
-        Color.defaultBlue.ignoresSafeArea()
+        Color.white.ignoresSafeArea()
         NavigationTitleView(title: "Messages", imageURL: mainBundle?.url(forResource: "Image", withExtension: "jpeg"))
     }
     
--- SelfUI/SelfUI/Configuration+Theme+Style.swift
+++ SelfUI/SelfUI/Configuration+Theme+Style.swift
@@ -60,3 +60,69 @@ struct CustomToggleStyle: ToggleStyle {
         }
     }
 }
+
+struct ImageToggleStyle: ToggleStyle {
+    var onColor: Color
+    var offColor: Color
+    var thumbImageOn: Image
+    var thumbImageOff: Image
+    
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            
+//            Spacer()
+
+            Button(action: {
+                configuration.isOn.toggle()
+            }) {
+                if !configuration.isOn {
+                    RoundedRectangle(cornerRadius: 16)
+                        .fill(configuration.isOn ? onColor : offColor)
+                        .frame(width: 50, height: 30)
+                        .overlay(
+                            thumbImageOn
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                .offset(x: configuration.isOn ? 10 : -10)
+    //                        Circle()
+    //                            .fill(thumbColor)
+    //                            .padding(3)
+    //                            .offset(x: configuration.isOn ? 10 : -10)
+                        )
+                        .animation(.easeInOut, value: configuration.isOn)
+                } else {
+                    RoundedRectangle(cornerRadius: 16)
+                        .fill(configuration.isOn ? onColor : offColor)
+                        .frame(width: 50, height: 30)
+                        .overlay(
+                            thumbImageOff
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                .offset(x: configuration.isOn ? 10 : -10)
+    //                        Circle()
+    //                            .fill(thumbColor)
+    //                            .padding(3)
+    //                            .offset(x: configuration.isOn ? 10 : -10)
+                        )
+                        .animation(.easeInOut, value: configuration.isOn)
+                }
+            }
+            
+            configuration.label
+        }
+    }
+}
+
+#Preview {
+    VStack {
+        Toggle(isOn: .constant(true)) {
+            Text("My Custom Switch")
+        }
+        .toggleStyle(ImageToggleStyle(onColor: .defaultGray, offColor: .defaultGray, thumbImageOn: Image("ic_self_logo", bundle: mainBundle), thumbImageOff: Image("ic_logo_quaternary", bundle: mainBundle)))
+        
+        Toggle(isOn: .constant(false)) {
+            Text("My Custom Switch")
+        }
+        .toggleStyle(ImageToggleStyle(onColor: .defaultGray, offColor: .defaultGray, thumbImageOn: Image("ic_self_logo", bundle: mainBundle), thumbImageOff: Image("ic_logo_quaternary", bundle: mainBundle)))
+    }
+}
