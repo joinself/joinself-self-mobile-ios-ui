9685c84358edeeefc0a389acf450845741fd5387
Long Pham <philong.bmt@gmail.com>
2025-10-29 13:09:21 +0700

    update some views in liveness camera

--- SelfUI/SelfUI/Liveness/LivenessChallengeView.swift
+++ SelfUI/SelfUI/Liveness/LivenessChallengeView.swift
@@ -10,8 +10,10 @@ import SwiftUI
 public struct LivenessChallengeView: View {
     
     @Binding var message: String
-    public init(message: Binding<String>) {
+    @Binding var position: Position
+    public init(message: Binding<String>, position: Binding<Position>) {
         self._message = message
+        self._position = position
     }
     
     public var body: some View {
@@ -19,6 +21,14 @@ public struct LivenessChallengeView: View {
             Spacer()
             ZStack {
                 //                Image("bg_turn_left", bundle: mainBundle)
+                backgroundView(for: position)
+                    .overlay {
+                        Text(message)
+                            .modifier(Heading4TextStyle())
+                            .padding()
+                            //.frame(maxWidth: .infinity)
+                    }
+                
                 Text(message)
                     .modifier(Heading4TextStyle())
                     .frame(maxWidth: .infinity)
@@ -32,16 +42,37 @@ public struct LivenessChallengeView: View {
                             )
                     )
                     .padding()
+                    .hidden()
                 
             }
+        }
+    }
+    
+    @ViewBuilder
+    private func backgroundView(for position: Position) -> some View {
+        switch position {
+        case .none:
+            Image("selfie_tip_background", bundle: mainBundle)
+        case .left:
+            Image("bg_turn_left", bundle: mainBundle)
+        case .right:
+            Image("bg_turn_right", bundle: mainBundle)
+        case .up:
+            Image("bg_lookup", bundle: mainBundle)
+            
+        case .down:
+            Image("bg_lookdown", bundle: mainBundle)
         }
     }
 }
 
 #Preview {
     VStack {
-        LivenessChallengeView(message: .constant("Move Closer"))
-        LivenessChallengeView(message: .constant("Move Further Away"))
+        LivenessChallengeView(message: .constant("Please position your face inside the frame"), position: .constant(.none))
+        LivenessChallengeView(message: .constant("Left"), position: .constant(.left))
+        LivenessChallengeView(message: .constant("Right"), position: .constant(.right))
+        LivenessChallengeView(message: .constant("Up"), position: .constant(.up))
+        LivenessChallengeView(message: .constant("Down"), position: .constant(.down))
     }
     .background(.gray)
 }
